a21a641505f74daeabe5e3e6e867c413984794dd
Andrew Chen <andrewchen0028@gmail.com>
2023-05-26 17:35:20 -0400

    IRLS with symmetrical fees & zero base fee

--- hex.py
+++ hex.py
@@ -2,8 +2,11 @@ import networkx as nx
 import numpy as np
 
 
+np.set_printoptions(precision=2, suppress=True, floatmode="fixed")
+
+
 def J(A, b, x):
-    return x.T @ A @ x + b.T @ np.abs(x)
+    return (1 / 2) * x.T @ A.T @ A @ x + b.T @ np.abs(x)
 
 
 G = nx.cycle_graph(6, create_using=nx.DiGraph())
@@ -18,20 +21,21 @@ G[0][3]["a"] = 1
 G[0][3]["b"] = 3000
 G[0][1]["b"] = G[1][2]["b"] = G[2][3]["b"] = 1
 
-C = nx.incidence_matrix(G, oriented=True).todense()
-d = np.array([[-10, 0, 0, 10, 0, 0]]).T
-
+# Get system matrices
 A = np.diag(list(nx.get_edge_attributes(G, "a").values()))
 b = np.array([list(nx.get_edge_attributes(G, "b").values())]).T
+C = nx.incidence_matrix(G, oriented=True).todense()
+d = np.array([[-10, 0, 0, 10, 0, 0]]).T
 Q = np.linalg.inv(A.T @ A)
 
-x = Q @ C.T @ np.linalg.pinv(C @ Q @ C.T) @ d
-print(x.T.round())
-print(f"J(x0) = {J(A, b, x)}")
+# Initial solution with unity weighting
+x = C.T @ np.linalg.pinv(C @ C.T) @ d
+print(f"\t\t\tx\t\t\tJ(x)")
+print(f"{x.T}\t{J(A, b, x)}")
 
-for i in range(3):
-    W = np.diag(np.array(A @ A @ x * x + b * np.abs(x)).T[0])
-    R = np.linalg.inv(W.T @ W)
+# Iterate
+for i in range(6):
+    W = A.T @ A + np.diag((b / np.abs(x)).T[0])
+    R = np.linalg.pinv(W)
     x = R @ C.T @ np.linalg.pinv(C @ R @ C.T) @ d
-    print(x.T.round())
-    print(f"J(x{i}) = {J(A, b, x)}")
+    print(f"{x.T}\t{J(A, b, x)}")
