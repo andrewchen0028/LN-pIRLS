b779fc623e9a02b2d215b00c2d03e2478a50d030
Andrew Chen <andrewchen0028@gmail.com>
2023-05-31 11:19:03 -0400

    add types to mpp

--- mpp.py
+++ mpp.py
@@ -3,62 +3,71 @@ import json, time
 from ortools.graph.python import min_cost_flow  # type: ignore
 
 
-def uniform_probability(s: int, d: int, a: int) -> float:
+# fmt: off
+class Lnid(str): pass
+class Orid(int): pass
+
+
+class McfArc:
+    def __init__(self, s: Orid, d: Orid, capacity: int, unit_cost: int) -> None:
+        self.s, self.d = s, d
+        self.capacity, self.unit_cost = capacity, unit_cost
+
+
+class Channel:
+    def __init__(self, capacity: int, prop_fee: int, base_fee: int,
+                 balance: int, lower_bound: int, upper_bound: int) -> None:
+        self.capacity, self.prop_fee, self.base_fee = capacity, prop_fee, base_fee
+        self.balance, self.lower, self.upper = balance, lower_bound, upper_bound
+# fmt: on
+
+
+def uniform_probability(s: Orid, d: Orid, a: int) -> float:
     """Computes uniform probability of a payment of amount `a` on a channel s-->d"""
-    c = channel_graph[s][d]
-    return float(c + 1 - a) / (c + 1)
+    return float(G[s][d].capacity + 1 - a) / (G[s][d].capacity + 1)
 
 
-def fee_msat(s: int, d: int, a: int) -> float:
+def fee_msat(s: Orid, d: Orid, a: int) -> float:
     """Computes the fees of a payment of amount `a` on a channel s-->d"""
-    base, rate = fee_graph[s][d]
     # Divide ppm by 1000 to be compatible with base_fee (measured in msat, not sats)
-    return base + a * rate / 1000
+    return G[s][d].base_fee + a * G[s][d].prop_fee / 1000
 
 
 # Amount (sats), linearization segments, HTLC size increment (sats)
 A, N, Q = 50e6, 5, 1000
 
 # Source and destination LNIDs
-S = "03efccf2c383d7bf340da9a3f02e2c23104a0e4fe8ac1a880c8e2dc92fbdacd9df"
-D = "021c97a90a411ff2b10dc2a8e32de2f29d2fa49d41bfbb52bd416e460db0747d0d"
+S = Lnid("03efccf2c383d7bf340da9a3f02e2c23104a0e4fe8ac1a880c8e2dc92fbdacd9df")
+D = Lnid("021c97a90a411ff2b10dc2a8e32de2f29d2fa49d41bfbb52bd416e460db0747d0d")
 channels = json.load(open("listchannels20220412_processed.json"))
 
 # Construct LNID set and find max channel capacity
 cmax = 0
-lnids: set[str] = set()
+lnids: set[Lnid] = set()
 for e in channels:
     lnids.update([e["s"], e["d"]])
     cmax = max(cmax, e["c"])  # FIXME: this ignores subsequent channel recombination
 
 # Map string LNIDs to integer ORIDs from [0, ..., n]
-lnid_to_orid: dict[str, int] = {lnid: orid for orid, lnid in enumerate(lnids)}
-orid_to_lnid: dict[int, str] = {orid: lnid for orid, lnid in enumerate(lnids)}
+lnid_to_orid: dict[Lnid, Orid] = {lnid: Orid(i) for i, lnid in enumerate(lnids)}
+orid_to_lnid: dict[Orid, Lnid] = {Orid(i): lnid for i, lnid in enumerate(lnids)}
 
-# Construct channel/fee graphs and linearized arcs
-# arcs:             stores linearized arcs      [(src, dest, capacity, unit_cost)]
-# channel_graph:    stores channel capacities   {src: {dest: capacity}}
-# fee_graph:        stores fees as a tuple      {src: {dest: (base_fee_msat, ppm)}}
-arcs: list[tuple[int, int, int, int]] = []
-channel_graph: dict[int, dict[int, int]] = {lnid_to_orid[lnid]: {} for lnid in lnids}
-fee_graph: dict[int, dict[int, tuple[int, int]]] = {
-    lnid_to_orid[lnid]: {} for lnid in lnids
-}
+# Initialize channel graph and linearized arcs
+arcs: list[McfArc] = []
+G: dict[Orid, dict[Orid, Channel]] = {lnid_to_orid[lnid]: {} for lnid in lnids}
+
 for e in channels:
-    # Get source, destination, and capacity
-    s, d, c = lnid_to_orid[e["s"]], lnid_to_orid[e["d"]], e["c"]
-
     # Put channels into channel graph, combining parallel channels
-    channel_graph[s][d] = channel_graph[s][d] + c if d in channel_graph[s] else c
-    fee_graph[s][d] = (e["b"], e["r"])  # FIXME: this ignores parallel channel fees
+    # FIXME: use optimal linearization
+    s, d, c = lnid_to_orid[e["s"]], lnid_to_orid[e["d"]], e["c"]
+    G[s][d] = Channel(c, e["r"], e["b"], e["b"], 0, c)
     for i in range(N):
-        # FIXME: use optimal linearization
-        arcs.append((s, d, int(c / (N * Q)), (i + 1) * int(cmax / c)))
+        arcs.append(McfArc(s, d, int(c / (N * Q)), (i + 1) * int(cmax / c)))
 
 # Invoke solver
 mcf = min_cost_flow.SimpleMinCostFlow()
 for arc in arcs:
-    mcf.add_arc_with_capacity_and_unit_cost(arc[0], arc[1], arc[2], arc[3])
+    mcf.add_arc_with_capacity_and_unit_cost(arc.s, arc.d, arc.capacity, arc.unit_cost)
 for orid in orid_to_lnid.keys():
     mcf.set_node_supply(orid, 0)
 mcf.set_node_supply(lnid_to_orid[S], int(A / Q))
@@ -73,13 +82,13 @@ if status != mcf.OPTIMAL:
     exit(1)
 
 # Create payment from all nonzero linearized flows
-payment: dict[tuple[int, int], int] = {}
+payment: dict[tuple[Orid, Orid], int] = {}
 for orid in range(mcf.num_arcs()):
     if mcf.flow(orid):
-        s: int = mcf.tail(orid)
-        d: int = mcf.head(orid)
+        s: Orid = mcf.tail(orid)
+        d: Orid = mcf.head(orid)
         f: int = mcf.flow(orid) * Q
-        payment[(s, d)] = f if (s, d) not in payment else payment[(s, d)] + f
+        payment[(s, d)] = payment[(s, d)] + f if (s, d) in payment else f
 
 print(
     f"Paying {(A / 100.0e6):4.2f} BTC from {lnid_to_orid[S]}({S[:7]}...) to"
@@ -99,7 +108,7 @@ for (s, d), f in payment.items():
     total_probability *= channel_probability
     print(
         f"({s:>5}, {d:>5})\t"
-        f"{f:>10} / {channel_graph[s][d]:>10}\t"
+        f"{f:>10} / {G[s][d].capacity:>10}\t"
         f"{channel_probability:^10.3f}\t"
         f"{fee:>10.3f}"
     )
